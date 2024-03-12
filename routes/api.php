34da74cf56eb76762aa55c24727d3d3f22a35c69
Zwandiboss08 <stefanussuwandi08@gmail.com>
2024-03-12 15:53:59 +0700

    add api for login, product, category

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +20,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// login api
+Route::post('/login', [\App\Http\Controllers\Api\AuthController::class, 'login']);
+
+// product api
+Route::apiResource('/api-products', ProductController::class)->middleware('auth:sanctum');
+// category api
+Route::apiResource('/api-categories', CategoryController::class)->middleware('auth:sanctum');
